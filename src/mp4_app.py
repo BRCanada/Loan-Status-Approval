9fadb377d1698645692a31ce546c23407730a4fd
Brandon Rose <brandonrosecanada@gmail.com>
2022-11-04 11:08:18 -0400

    add src/, Model_Pipeline.ipynb

---
+++ src/mp4_app.py
@@ -0,0 +1,31 @@
+# import Flask and jsonify
+from flask import Flask, jsonify
+
+# import Resource, Api and reqparser
+from flask_restful import Resource, Api, reqparse
+
+# import pandas, numpy, pickle
+import pandas as pd
+import numpy
+import pickle
+
+#definitions
+app = Flask(__name__)    
+api = Api(app)
+
+model = pickle.load(open('model.p', 'rb'))
+
+class Scoring(Resource):
+    def post(self):
+        json_data = request.get_json()
+        df = pd.DataFrame(json_data.values(), index=json_data.keys()).transpose()
+        
+        #Make predictions
+        res = model.predict(df)
+        return res.tolist()
+    
+#Make endpoint
+api.add_resource(Scoring, '/scoring')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='4242', debug=True)
